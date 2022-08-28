eb8e1424e47c3e9d35d8ecc425a2b5a8ae4a1f83
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2022-08-28 22:07:37 +0300

    deploy to firebase

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,81 @@ let path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const isProd = process.env.NODE_ENV === 'production';
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetPlugin = require("css-minimizer-webpack-plugin");
+let mode = "development"
 
+if (isProd) { // Режим production, если 
+  // при запуске вебпака было указано --mode=production
+  mode = 'production';
+}
+
+const publicPath = process.env.PUBLIC_URL || '/';
+
+
+// console.log(publicPath)
+console.log(mode)
+// const optimization = () => {
+//   const config = {
+//     splitChunks: {
+//       chunks: "all",
+//     },
+//   };
+
+//   if (isProd) {
+//     config.minimizer = [
+//       new OptimizeCssAssetPlugin(),
+//       new TerserWebpackPlugin(),
+//     ];
+//   }
+
+//   return config;
+// };
 
 module.exports = {
-   
+  
+  mode,
+
     // собираем всё в режиме разработки
-    mode: "development",
+   
+    
     // указываем тот файл,который является входным.Т.е мы указываем путь к основному файлу,а webpack уже самостоятельно соединяет все модули
     entry: {
-      // 
-      main: ["./src/index.jsx"],
-    },
+ //  
+ main: ['@babel/polyfill',"./src/index.jsx"]
+
+    }
+     
+     
+    ,
+   
+
     // указываем куда нужно складывать все результаты webpack'a
     output: {
       filename: "[name].[contenthash].js",
-      // куда это всё складывать?Путь(дорожка) указавается в поле path
-      // для того чтобы использовать данное поле воспользуемся встроенным модулем path(он прописан вверху)
-  
-      path: path.resolve(__dirname, 'dist'),
+      
+      path: path.resolve(__dirname, 'public'),
+      publicPath
+      
+      
     },resolve: {
         extensions: [".js", ".json", '.jsx', ".png"],
       },
+    //  optimization: optimization(),
       devServer: {
+        // чтобы не было not Get beer/1  
         historyApiFallback: true,
-        static: "./",
+        // static: "./",
         port: 4200,
+        //для Post-запросов, чтобы страница не перезагружалась
         liveReload: false
       },
    
       plugins: [
         new HTMLWebpackPlugin({
-          template: "./index.html",
+          template: "./index.html"
         }),  new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: "[name].[contenthash].css",
